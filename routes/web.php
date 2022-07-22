244e0082bb28534514f56c0b9e2b60e1cedb1392
huy <huy@aimfirst.com>
2022-07-22 11:17:37 +0700

    new new

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,15 @@ use Kutia\Larafirebase\Facades\Larafirebase;
 */
 
 Route::get('/', function () {
-//server key : AAAAeFNUr_c:APA91bHpOxvq56UoC6i7yAWyPZcz769vKgyM4kVTk5FkbaXUy1jumZ-hZKkqWbys29MNP8q21kNM9_DeFzzsCwytc_3eZLacR2dASViNrCOeMlUv4vpzga1_PzfrqA6hCOxw1QlTI_88
+//server key : 'AAAAeFNUr_c:APA91bHpOxvq56UoC6i7yAWyPZcz769vKgyM4kVTk5FkbaXUy1jumZ-hZKkqWbys29MNP8q21kNM9_DeFzzsCwytc_3eZLacR2dASViNrCOeMlUv4vpzga1_PzfrqA6hCOxw1QlTI_88'
 //    $deviceTokens = User::whereDay('birthday', now()->format('d'))
 //        ->whereMonth('birthday', now()->format('m'))
 //        ->pluck('device_token')
 //        ->toArray();
     PushNotificationJob::dispatch('sendBatchNotification', [
-        'd0508ff948131aea4a151cc2d532cd3ddd2b80ef5016ef7f87d2b5df4d6d629a',
+        '39a56088-e727-468c-940c-b8fd60428f41',
+//        'd0508ff948131aea4a151cc2d532cd3ddd2b80ef5016ef7f87d2b5df4d6d629a',
+    //team id: 336TBZ25TJ
         [
             'topicName' => '/topics/test',
             'title' => 'Chúc mứng sinh nhật',
@@ -35,3 +37,61 @@ Route::get('/', function () {
 Route::get('2',function(){
     Larafirebase::withTitle('Test Title')->withBody('Test body')->sendNotification('d0508ff948131aea4a151cc2d532cd3ddd2b80ef5016ef7f87d2b5df4d6d629a');
 });
+Route::get('3', function () {
+    /* We are using the sandbox version of the APNS for development. For production
+    environments, change this to ssl://gateway.push.apple.com:2195 */
+    $apnsServer = 'ssl://gateway.sandbox.push.apple.com:2195';
+    /* Make sure this is set to the password that you set for your private key
+    when you exported it to the .pem file using openssl on your OS X */
+    $privateKeyPassword = '1234';
+    /* Put your own message here if you want to */
+    $message = 'Welcome to iOS 7 Push Notifications';
+    /* Pur your device token here */
+    $deviceToken =
+        'd0508ff948131aea4a151cc2d532cd3ddd2b80ef5016ef7f87d2b5df4d6d629a';
+    /* Replace this with the name of the file that you have placed by your PHP
+    script file, containing your private key and certificate that you generated
+    earlier */
+    $pushCertAndKeyPemFile = public_path('cer/pushcert.pem');
+    $stream = stream_context_create();
+    stream_context_set_option($stream,
+        'ssl',
+        'passphrase',
+        $privateKeyPassword);
+    stream_context_set_option($stream,
+        'ssl',
+        'local_cert',
+        $pushCertAndKeyPemFile);
+    $connectionTimeout = 20;
+    $connectionType = STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT;
+    $connection = stream_socket_client($apnsServer,
+        $errorNumber,
+        $errorString,
+        $connectionTimeout,
+        $connectionType,
+        $stream);
+    if (!$connection) {
+        echo "Failed to connect to the APNS server. Error no = $errorNumber<br/>";
+        exit;
+    } else {
+        echo "Successfully connected to the APNS. Processing...</br>";
+    }
+    $messageBody['aps'] = array('alert' => $message,
+        'sound' => 'default',
+        'badge' => 2,
+    );
+    $payload = json_encode($messageBody);
+    $notification = chr(0) .
+        pack('n', 32) .
+        pack('H*', $deviceToken) .
+        pack('n', strlen($payload)) .
+        $payload;
+    $wroteSuccessfully = fwrite($connection, $notification, strlen($notification));
+    if (!$wroteSuccessfully) {
+        echo "Could not send the message<br/>";
+    } else {
+        echo "Successfully sent the message<br/>";
+    }
+    fclose($connection);
+});
+
